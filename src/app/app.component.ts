12c9150c6bcba1403864ce0c07d2a8cdac60376c
Arthur Lavrischev <ar2r13lawrence@gmail.com>
2018-04-16 18:28:30 +0300

    socnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { WalletService } from './services/wallet.service'
 import { Component } from '@angular/core'
+import { FacebookService } from 'ng2-facebook-sdk'
+import { SocialLoginService, Provider } from 'ngx-social-login'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [WalletService]
+  providers: []
 })
 export class AppComponent {
-  creds = {}
-  wallet = {}
-  loading : boolean = false
 
-  constructor (private walletService : WalletService) {}
+    constructor (private fb : FacebookService, private test : SocialLoginService) {
+        // fb.init({
+        //   appId: '751488778286304',
+        //   xfbml: true,
+        //   version: 'v2.8'
+        // })
+    }
 
-  async newWallet (seed : string) {
-    this.loading = true
-    this.wallet = await this.walletService.walletByCredentials(this.creds.pass, seed)
-    this.loading = false
-  }
+    async facebook () {
+        this.test.login(Provider.FACEBOOK).subscribe(user => console.log(user))
+        // const status = await this.fb.getLoginStatus()
+        // if (status !== 'connected') {
+        //     await this.fb.login({
+        //         enable_profile_selector: true,
+        //         return_scopes: true,
+        //         scope: 'public_profile,email'
+        //     })
+        // }
+        // const userData = await this.fb.api('/me')
+        // console.dir(userData)
+    }
+
+    async google () {
+        this.test.login(Provider.GOOGLE).subscribe(user => console.log(user))
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { FacebookService } from 'ng2-facebook-sdk'
+
+import { NgxSocialLoginModule } from 'ngx-social-login'
 
 import { AppComponent } from './app.component'
 
@@ -11,9 +14,21 @@ import { AppComponent } from './app.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgxSocialLoginModule.init(
+            {
+                google: {
+                    client_id: '1019497029966-nkmnpmcc0nd660sedoi66esfiafv3r6p.apps.googleusercontent.com'
+                },
+                facebook: {
+                    initOptions: {
+                        appId: '751488778286304'
+                    }
+                }
+            }
+        )
   ],
-  providers: [],
+  providers: [FacebookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
